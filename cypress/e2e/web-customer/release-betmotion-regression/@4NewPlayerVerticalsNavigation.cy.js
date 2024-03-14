2824075daaae0c76da16a58bf61c5bc2b9f122bc
Ruan Felipe <ruananhanguera@gmail.com>
2024-03-14 19:02:02 -0300

    verticalMain-bevisible

--- cypress/e2e/web-customer/release-betmotion-regression/@4NewPlayerVerticalsNavigation.cy.js
+++ cypress/e2e/web-customer/release-betmotion-regression/@4NewPlayerVerticalsNavigation.cy.js
@@ -11,19 +11,18 @@ describe('navigate on the verticals with a new player', () => {
     it('must navigate on the verticals', () => {
         cy.viewport(1920, 1080)
         cy.visit('https://bmsite-staging.salsaomni.com/br/', { failOnStatusCode: false })
-        cy.get(VERTICALS_MENU.verticalMain).should('contain.text', 'Aposta esportiva', 'Slot', 'Cassino', 'Bingo')
+        cy.get(VERTICALS_MENU.verticalMain).should('be.visible').and('contain.text', 'Aposta esportiva', 'Slot', 'Cassino', 'Bingo')
         cy.get(VERTICALS_MENU.verticalMain).contains('Aposta esportiva').click()
         cy.url().should('eq', VERTICALS_MENU.sportsBettingVerticalURL)
         cy.get(VERTICALS_MENU.verticalMain).contains('SLOT').click()
         cy.url().should('eq', VERTICALS_MENU.slotVerticalUrl)
         cy.get(VERTICALS_MENU.verticalMain).contains('CASSINO').click()
-        cy.url().should('eq', VERTICALS_MENU.casinoVerticalUrl )
+        cy.url().should('eq', VERTICALS_MENU.casinoVerticalUrl)
         cy.get(VERTICALS_MENU.verticalMain).contains('BINGO').click()
         cy.url().should('eq', VERTICALS_MENU.bingoVertical)
         cy.get(VERTICALS_MENU.verticalMain).contains('PROMOÇÕES').click()
         cy.url().should('eq', VERTICALS_MENU.promotionsVerticalUrl)
 
-
     })
-   
+
 })
